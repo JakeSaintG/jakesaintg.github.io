22b05e4df7e2cb67332f0d0ed6327525d8ce79d0
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 20:44:15 -0400

    added animation, more content, fixed navigation in mobile

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function showHide() {
+    var x = document.getElementById("myOthernav");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
