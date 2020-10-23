a199b1bce81f08b0532f74de34f9e53c255b949e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-23 05:22:28 -0400

    added ability to make post on blog page

---
+++ js/blogpost.js
@@ -0,0 +1,40 @@
+//To do: validate data to ensure that something must be in the text boxes
+//To do: Be able to make mulitple posts by resetting form on submit and allowing it to create another div
+
+function populatePost() {
+const title = document.getElementById("posttitle").value;
+console.log(title)
+const author = document.getElementById("post_author").value;
+console.log(author)
+const post = document.getElementById("post_content").value;
+console.log(post)
+
+let today = new Date(); // "new" operator prevents the date from being a string 
+let d = today.getDate();
+let m = today.getMonth()+1; //return the month starting at 0, +1 is used to simulate returning the month starting at 1.
+let y = today.getFullYear();
+
+if(d<10) 
+{
+    d='0'+d; //adds a zero to the day if it is a single digit
+} 
+
+if(m<10) 
+{
+    m='0'+m;
+} 
+
+today = `${m}/${d}/${y}`;
+console.log(today);
+
+document.getElementById("post").innerHTML = `<article class="post_full">
+<div class="post_head">
+    <h1 class="post_title">${title}</h1>
+    <div class="post_details">
+        <h3 class="post_author">By: ${author}</h3>
+        <h3 class="post_date">${today}</h3>
+    </div>
+</div>
+<p class="post_content">${post} </p>
+</article>`
+}
